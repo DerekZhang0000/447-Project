f067bd977a690ade6651322087b8436d025832e8
Derek Zhang <derekzhang0000@gmail.com>
2024-04-23 23:11:34 -0500

    a

--- main.py
+++ main.py
@@ -17,5 +17,68 @@ def query():
     connection.close()
     return render_template('result.html', rows=rows)
 
+@app.route('/get_military_ships', methods=['GET'])
+def get_military_ships():
+    connection = sqlite3.connect('boats.db')
+    cursor = connection.cursor()
+    cursor.execute("""SELECT *
+                      FROM 'Military Ship'""")
+    rows = cursor.fetchall()
+    connection.close()
+    return render_template('result.html', rows=rows)
+
+@app.route('/count_military_ships_by_builder', methods=['GET'])
+def count_military_ships_by_builder():
+    connection = sqlite3.connect('boats.db')
+    cursor = connection.cursor()
+    cursor.execute("""SELECT b.'Company Name', COUNT(ms.'Ship Name')
+                      FROM 'Military Ship' AS ms
+                      JOIN Builder b ON ms.Builder = b.'Company Name'
+                      GROUP BY b.'Company Name'""")
+    rows = cursor.fetchall()
+    connection.close()
+    return render_template('result.html', rows=rows)
+
+
+@app.route('/list_all_ships_and_dates', methods=['GET'])
+def list_all_ships_and_dates():
+    connection = sqlite3.connect('boats.db')
+    cursor = connection.cursor()
+    cursor.execute("""SELECT s.'Ship Name', COALESCE(s.'Construction Date', ms.'Construction Date') AS 'Construction Date'
+                      FROM Ship AS s
+                      LEFT JOIN 'Military Ship' AS ms ON s.'Ship Name' = ms.'Ship Name'
+                      UNION
+                      SELECT ms.'Ship Name', ms.'Construction Date'
+                      FROM 'Military Ship' AS ms
+                      WHERE ms.'Ship Name' NOT IN (SELECT s.'Ship Name' FROM Ship s)""")
+    rows = cursor.fetchall()
+    connection.close()
+    return render_template('result.html', rows=rows)
+
+
+@app.route('/query_specific_location', methods=['POST'])
+def query_specific_location():
+    location = request.form['location']
+    connection = sqlite3.connect('boats.db')
+    cursor = connection.cursor()
+    cursor.execute(f"""SELECT ms.'Ship Name', Builder
+                       FROM 'Military Ship' AS ms
+                       WHERE ms.'Deployment Location' = '{location}'""")
+    rows = cursor.fetchall()
+    connection.close()
+    return render_template('result.html', rows=rows)
+
+@app.route('/query_specific_date', methods=['POST'])
+def query_specific_date():
+    date = request.form['date']
+    connection = sqlite3.connect('boats.db')
+    cursor = connection.cursor()
+    cursor.execute(f"""SELECT ms.'Ship Name', Type
+                       FROM 'Military Ship' AS ms
+                       WHERE ms.'Commission Date' = '{date}'""")
+    rows = cursor.fetchall()
+    connection.close()
+    return render_template('result.html', rows=rows)
+
 if __name__ == '__main__':
     app.run(debug=True)
